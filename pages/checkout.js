88e4138614fadc738cafd354799d1f8217c61de8
Tavern <tavernkinder711@gmail.com>
2026-02-27 07:53:24 -0700

    Added remove and edit item in cart functions

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,27 +1,31 @@
 import {products} from '../data/products-data.js';
 import {cart} from '../cart/cart.js';
-
+cart.cartLength();
 const main = document.getElementById("checkout-main")
 let fullPrice = 0;
 
 function renderCartPage() {
-    main.innerHTML = `
-    <div class="checkout-container">
-        <section id="cartItems" class="checkout-column checkout-left">
-            <h1 class="text-2xl font-bold mt-8 mb-4">Your Cart</h1>
-            <!-- Cart items will be dynamically added here -->
-        </section>
-        <aside id="checkoutBox" class="checkout-column checkout-right">
-            <h2 class="text-xl font-bold">Total: $<span id="totalPrice">0.00</span></h2>
-            <button class="mt-4 bg-amazon-yellow text-white px-4 py-2 rounded hover:bg-amazon-yellow-hover" id="checkoutButton">Proceed to Checkout</button>
-        </aside>
-    </div>`;
-    const cartItems = document.getElementById('cartItems');
-    fillCartAndCost();
-    const checkoutBTN = document.getElementById('checkoutButton');
-    checkoutBTN.addEventListener('click', () =>{
-        renderCheckoutPage();
-    })
+    if(cart.items.length === 0){main.innerHTML = 
+        '<div class-"checkout-container"><h1 class="text-2xl font-bold mt-8 mb-4">Your cart is empty</h1></div>'}
+    else{
+        main.innerHTML = `
+        <div class="checkout-container">
+            <section id="cartItems" class="checkout-column checkout-left">
+                <h1 class="text-2xl font-bold mt-8 mb-4">Your Cart</h1>
+                <!-- Cart items will be dynamically added here -->
+            </section>
+            <aside id="checkoutBox" class="checkout-column checkout-right">
+                <h2 class="text-xl font-bold">Total: $<span id="totalPrice">0.00</span></h2>
+                <button class="mt-4 bg-amazon-yellow text-white px-4 py-2 rounded hover:bg-amazon-yellow-hover" id="checkoutButton">Proceed to Checkout</button>
+            </aside>
+        </div>`;
+        const cartItems = document.getElementById('cartItems');
+        fillCartAndCost();
+        const checkoutBTN = document.getElementById('checkoutButton');
+        checkoutBTN.addEventListener('click', () =>{
+            renderCheckoutPage();
+        })
+    }
 }
 
 function renderCheckoutPage() {
@@ -78,24 +82,41 @@ function fillCartAndCost(){
             <div class="flex items-center">
                 <img src="${item.image}" alt="${item.name}" class="w-16 h-16 object-cover rounded mr-4">
                 <div>
-                <div>
-                    <h2 class="text-lg font-bold">${item.name}</h2>
-                    <p class="text-gray-600">$${item.price.toFixed(2)} X ${item.amount}</p>
+                    <div>
+                        <h2 class="text-lg font-bold">${item.name}</h2>
+                        <p class="text-gray-600">$${item.price.toFixed(2)} X ${item.amount}</p>
+                    </div>
+                    <div>
+                        <p class="text-amazon-yellow font-bold mt-2">Total: $${item.totalPrice}</p>
+                    </div>
+                    </div>
+                    <div>
+                        <p class="checkout-desc truncate"> ${item.description} </p>
+                    </div>
                 </div>
-                <div>
-                    <p class="text-amazon-yellow font-bold mt-2">Total: $${item.totalPrice}</p>
+                <div class="edit-item">
+                    <button id='edit-${item.id}' class='edit'>edit</button>
+                    <button id='remove-${item.id}' class='remove'>remove</button>
                 </div>
-            </div>
-            <div>
-             <p> ${item.description}
             </div>
         </div>
         `;
-
+    });
+    cart.items.forEach(item => {
+        let currentRemove = document.getElementById(`remove-${item.id}`)
+        currentRemove.addEventListener('click', () => {
+            cart.removeItem(item.id);
+            renderCartPage();
+        })
+        let currentEdit = document.getElementById(`edit-${item.id}`)
+        currentEdit.addEventListener('click', () => {
+            let editAmount = prompt("What would you like to Change the amount to?", item.amount)
+            cart.editItem(item.id, editAmount);
+            renderCartPage();
+        })
     });
     const totalPriceElement = document.getElementById('totalPrice');
     if (totalPriceElement) totalPriceElement.textContent = fullPrice.toFixed(2);
-
 }
 
 renderCartPage();
--- pages/products-page.js
+++ pages/products-page.js
@@ -11,9 +11,9 @@ function renderProductsPage() {
         <div class="product-card shadow-lg rounded-lg p-4 m-4 bg-white col-span-1">
         <img src="${currentProduct.image}" alt="${currentProduct.name}" class="w-full h-48 object-cover rounded-t-lg">
             <h2 class="text-xl font-bold mt-4">${currentProduct.name}</h2>
-            <p class="text-gray-600 mt-2">${currentProduct.description}</p>
-            <p class="text-green-500 mt-2 font-bold">$${currentProduct.toDollar ? currentProduct.toDollar() : (currentProduct.price/100).toFixed(2)}</p>
-            <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 buyButton" id="${currentProduct.id}">Add to Cart</button>
+            <p class="text-gray-600 mt-2 truncate">${currentProduct.description}</p>
+            <p class="text-amazon-orange mt-2 font-bold">$${currentProduct.toDollar ? currentProduct.toDollar() : (currentProduct.price/100).toFixed(2)}</p>
+            <button class="mt-4 bg-amazon-yellow text-white px-4 py-2 rounded hover:bg-amazon-yellow-hover buyButton" id="${currentProduct.id}">Add to Cart</button>
         </div>
      ` ;
     }
